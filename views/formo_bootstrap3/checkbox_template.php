c20537c3031e2755bcf19a013f78f36ab0dfdc83
Kenny <JustLikeIcarus@gmail.com>
2013-09-27 03:34:35 -0400

    Create checkbox_template.php

---
+++ views/formo_bootstrap3/checkbox_template.php
@@ -0,0 +1,13 @@
+<div class="field form-group formo-<?=$field->get('driver')?><?php if ($error = $field->error()) echo ' error'; ?>" id="field-container-<?=$field->alias()?>">
+	<div class="checkbox">	
+		<label>
+			<?=$field->open().$field->html().$field->render_opts().$field->close()?> 
+			<span class="checkbox-label"><?=$field->label()?></span>
+		</label>
+	</div>
+	<?php if ($msg = $field->error()): ?>
+		<span class="help-block"><?=$msg?></span>
+	<?php elseif ($msg = $field->get('message')): ?>
+		<span class="help-block"><?=$msg?></span>
+	<?php endif; ?>
+</div>
