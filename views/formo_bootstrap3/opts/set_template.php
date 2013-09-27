3fe1a0c2bef4041349514903a174c76a3f64fbcd
Kenny <JustLikeIcarus@gmail.com>
2013-09-27 03:38:53 -0400

    Create set_template.php

---
+++ views/formo_bootstrap3/opts/set_template.php
@@ -0,0 +1,8 @@
+<div class="checkbox opts">
+<?php foreach ($opts as $key => $opt): ?>
+	<label>
+		<input type="checkbox" name="<?=$field->name()?>[]" value="<?=$key?>" <?php if (in_array($key, explode(',', $field->val()))) echo ' checked="checked"'; ?> /> 
+		<span class="opt-label"><?=$opt?></span>
+	</label>
+<?php endforeach; ?>
+</div>
